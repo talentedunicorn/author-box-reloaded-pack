e31999ca50d6f22344dc4f9d902a7bbe09e5ab51
ipublicis <ipublicis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-28 19:19:02 +0000

    First release
    git-svn-id: https://plugins.svn.wordpress.org/author-box-reloaded-pack/trunk@234853 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ xing.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Plugin Name: Author Box Reloaded - Xing Contact
+Plugin URI: http://wordpress.org/extended/plugins/author-box-2/
+Description: Adds Xing to your profile. Just need to insert your UserID. Requires Author Box Reloaded 2.0 or greater.
+Version: 1.0
+Author: Sven Schneider <schneider.sven@permanentmedia.de>
+Author URI: http://permanentmedia.de/
+Donate link: http://smsh.me/7kit
+License: GNU GPL v3 or later
+
+    Copyright (C) 2010 iPublicis!COM
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+******************************************
+
+HOW TO:
+
+On the header above:
+
+    1. Change the name in Plugin Name and Description. 
+    2. Change Plugin URI, Version, Author, Author URI and Donate link.
+    3. Keep the rest AS IS
+
+On the code area:
+
+    1. Change the function name but keep the "_authorbox_add_sites" portion.
+		a) We usually use the name text for it, i.e. for "Wordpress.Org" you'll set "wordpress_org" or something like it.
+    2. Change the $known_sites key and the "favicon" and "url" variables. 
+		a) The key is used in the profile form so use the same text you used in the Plugin Name in header area.
+        b) Don't forget that the $known_site url must have the text "USERNAME" in it in order to work properly.
+    3. Change the add_filter() function to include your "_authorbox_add_sites" function.
+
+To make it available to everyone follow the rules defined at [Wordpress.Org](http://http://wordpress.org/extend/plugins/about/).
+This is just another Wordpress plugin that will made some data available for Author Box Reloaded.
+
+Thank you for your help and contribution.
+
+*/
+
+$this_contact_name = 'Xing'; 				// CHANGE this variable to the service name
+$plugins = get_option('active_plugins');
+$required_plugin = 'author-box-2/authorbox.php';
+if ( !in_array( $required_plugin , $plugins ) ) {
+	$wpfr = '<a href="http://wordpress.org/extend/plugins/author-box-2/" target="_blank">Author Box Reloaded</a>';
+	$dieMessage  = sprintf( 'The %s plugin must be installed and active.', $wpfr );
+	$notice = "<div id=\"message\" class=\"error fade\"><p><strong>".$this_contact_name." Contact</strong></p><p>".$dieMessage."</p></div>\n";
+	add_action('admin_notices', create_function( '', "echo '$notice';" ) );
+}
+
+function xing_authorbox_add_sites( $known_sites ) {					// CHANGE the function prefix name
+	$known_sites['Xing'] = array(													// CHANGE the key name
+		'favicon' => plugin_dir_url( __FILE__ ) . 'images/xing.png',		// CHANGE the image name
+		'url' => 'http://www.xing.com/profile/USERNAME',					// CHANGE the service URI 
+  );
+  return $known_sites;
+}
+add_filter('authorbox_known_sites','xing_authorbox_add_sites',10,1);		// CHANGE the function prefix name
+
+?>
